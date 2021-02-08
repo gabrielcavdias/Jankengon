879f6e96ac71162786a66d37d23f8c31ec4f0d70
Helio Gabriel <64484809+gabrielcavdias@users.noreply.github.com>
2021-02-07 21:24:13 -0300

    Atualizações e Score

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
-gon = document.getElementById('gon')
+let gon = document.getElementById('gon')
+let score = 0
 function janken(ppt){
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -10,35 +11,44 @@ function janken(ppt){
     gon.innerHTML=`<p>Prepare-se...</p> <p>Primeiro é pedra!</p><img src="img/primeiropedra.jpg">`
     document.querySelector("audio").play()
     /* 0 = Pedra   1 = Papel   2 = tesoura*/
-    setTimeout(function(){
+    setTimeout(() => {
         if(gontry===0){
                 if(mytry===0){
                     gon.innerHTML = '<p>Empate</p>'
                 }else if(mytry===1){
                     gon.innerHTML = '<p>Você venceu!!!</p>'
+                    score+=1
                 }else{
                     gon.innerHTML = '<p>Gon venceu!!!</p>'
+                    score-=1
                 }
                 gon.innerHTML+='<p>E Gon escolheu Pedra</p> <img src="img/pedra.jpg">'
             }else if(gontry==1){
                 if(mytry===0){
                     gon.innerHTML = '<p>Gon venceu!!!</p>'
+                    score-=1
                 }else if(mytry===1){
                     gon.innerHTML = '<p>Empate</p>'
                 }else{
                     gon.innerHTML = '<p>Você venceu!!!</p>'
+                    score+=1
                 }
                 gon.innerHTML+='<p>E Gon escolheu Papel</p> <img src="img/papel.jpg">'
             }else if(gontry===2){
                 if(mytry===0){
                     gon.innerHTML = '<p>Você venceu!!!</p>'
+                    score+=1
                 }else if(mytry===1){
                     gon.innerHTML = '<p>Gon venceu!!!</p>'
+                    score-=1
                 }else{
                     gon.innerHTML = '<p>Empate</p>'
                 }
                 gon.innerHTML+='<p>E Gon escolheu Tesoura</p> <img src="img/tesoura.jpg">'
             }
-    }, 4000)
+            let innerScore = document.querySelector('.score>p+p')
+            innerScore.innerText = score
+    }
+    , 4000)
     
 }
